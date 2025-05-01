1579712ac916914199733bb91b99f4a2281c9171
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-01 09:01:59 +0530

    Update SmartCalculator.java

--- Java_OOP_Concepts/Q7Calculator/SmartCalculator.java
+++ Java_OOP_Concepts/Q7Calculator/SmartCalculator.java
@@ -2,6 +2,11 @@ package Java_OOP_Concepts.Q7Calculator;
 
 public class SmartCalculator extends BasicCalculator{
 
+    // @Override
+    // public int add(int a, int b){   throw error 
+    //     return a + b;
+    // }
+    
     public int Substract(int a, int b) {
         return a - b;
     }
