c21c07c4ac670e145b67c635b456436d573e67ed
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 21:03:41 +0530

    Update Main.java

--- CollectionsFrameWork/Q5ProductCatlog/Main.java
+++ CollectionsFrameWork/Q5ProductCatlog/Main.java
@@ -14,12 +14,12 @@ public class Main {
         sortedEntries.sort((e1, e2) -> {
             Product p1 = e1.getValue();
             Product p2 = e2.getValue();
-            int ratingComparison = Double.compare(p1.rating, p2.rating);
+            int ratingComparison = Double.compare(p2.rating, p1.rating);
             if (ratingComparison != 0) return ratingComparison;
             return p2.name.compareTo(p1.name);
         });
 
-        for (Map.Entry<Integer, Product> entry : products.entrySet()) {
+        for (Map.Entry<Integer, Product> entry : sortedEntries.entrySet()) {
             System.out.println(entry.getKey() + " -> Category: " + entry.getValue().name);
         }
     }
