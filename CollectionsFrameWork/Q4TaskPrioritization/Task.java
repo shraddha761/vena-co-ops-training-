ebdd6a72cbf10813309893275e8ea6e9a7421adc
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 22:10:03 +0530

    Update Task.java

--- CollectionsFrameWork/Q4TaskPrioritization/Task.java
+++ CollectionsFrameWork/Q4TaskPrioritization/Task.java
@@ -3,15 +3,20 @@ package CollectionsFrameWork.Q4TaskPrioritization;
 import java.time.LocalDateTime;
 
 public class Task {
-    String taskName;
-    int priority;
-    LocalDateTime deadline;
+    private String taskName;
+    private int priority;
+    private LocalDateTime deadline;
 
     Task(String taskName, int priority, LocalDateTime deadline) {
         this.taskName = taskName;
         this.priority = priority;
         this.deadline = deadline;
     }
+    
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
     @Override
     public String toString() {
         return name + " | Priority: " + priority + " | Deadline: " + deadline;
