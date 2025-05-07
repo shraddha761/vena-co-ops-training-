826014b960525f5af6a7d56c1aa05787b0a053dd
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 20:35:55 +0530

    Update Student.java

--- CollectionsFrameWork/Q1StudentManagement/Student.java
+++ CollectionsFrameWork/Q1StudentManagement/Student.java
@@ -1,17 +1,25 @@
 package CollectionsFrameWork.Q1StudentManagement;
 
 public class Student {
+    private static int counter = 1;
     int id;
     String name;
     int age;
     double grade;
     String branch;
 
-    Student(int id, String name, int age, double grade, String branch) {
-        this.id = id;
+    Student(String name, int age, double grade, String branch) {
+        this.id = counter++;
         this.name = name;
         this.age = age;
         this.grade = grade;
         this.branch = branch;
+    }
+    public double getGrade() {
+        return grade;
+    }
+
+    public String getBranch() {
+        return branch;
     }
 }
