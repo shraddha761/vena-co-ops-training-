2ce008f406cc7a7606b2e3ae55b8edd9f085cd0d
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 21:23:20 +0530

    Update Leaderboard.java

--- CollectionsFrameWork/Q9LeaderboardTracker/Leaderboard.java
+++ CollectionsFrameWork/Q9LeaderboardTracker/Leaderboard.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 
 public class Leaderboard {
     Comparator<Player> comparator = (p1, p2) ->{
-        int score = Integer.compare(p2.score , p2.score);
+        int score = Integer.compare(p2.score , p1.score);
         return score;
     };
     private TreeSet<Player> leaderboard = new TreeSet<>(comparator);
