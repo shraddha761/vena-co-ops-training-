4d0ca09846f79e647547ef00932eba9ccba53cc0
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 22:03:32 +0530

    Update Employee.java

--- CollectionsFrameWork/Q3DepartmentDirectory/Employee.java
+++ CollectionsFrameWork/Q3DepartmentDirectory/Employee.java
@@ -1,13 +1,24 @@
 package CollectionsFrameWork.Q3DepartmentDirectory;
 
 public class Employee {
-    String name;
-    String department;
-    double salary;
+    private String name;
+    private String department;
+    private double salary;
 
     public Employee(String name, String department, double salary) {
         this.name = name;
         this.department = department;
         this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 }
