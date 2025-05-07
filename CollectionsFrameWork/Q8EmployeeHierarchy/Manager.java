6e3f406a2fff1f24771d86e5e177bb99e2261cfd
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 21:19:38 +0530

    Update Manager.java

--- CollectionsFrameWork/Q8EmployeeHierarchy/Manager.java
+++ CollectionsFrameWork/Q8EmployeeHierarchy/Manager.java
@@ -6,4 +6,25 @@ public class Manager {
     Manager(String name){
         this.name = name;
     }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Manager)) return false;
+        Manager manager = (Manager) o;
+        return Objects.equals(name, manager.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
