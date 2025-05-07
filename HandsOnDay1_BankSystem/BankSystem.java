e95a438f60099a84e14fc4fe9b75414e4163293b
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-07 13:19:10 +0530

    Update BankSystem.java

--- HandsOnDay1_BankSystem/BankSystem.java
+++ HandsOnDay1_BankSystem/BankSystem.java
@@ -3,7 +3,7 @@ package HandsOnDay1_BankSystem;
 public class BankSystem {
     public static void main(String[] args) {
         SavingsAccount savings = new SavingsAccount("SA123", 1000, 0.05);
-        CheckingAccount checking = new CheckingAccount("CA456", 500, 200, 35);
+        CurrentAccount current = new CurrentAccount("CA456", 500, 200, 35);
 
         System.out.println("=== Savings Account ===");
         savings.display();
@@ -12,12 +12,12 @@ public class BankSystem {
         savings.applyInterest();
         savings.display();
 
-        System.out.println("\n=== Checking Account ===");
-        checking.display();
-        checking.deposit(300);
-        checking.withdraw(900);
-        checking.withdraw(200);
-        checking.applyInterest();
-        checking.display();
+        System.out.println("\n=== Current Account ===");
+        current.display();
+        current.deposit(300);
+        current.withdraw(900);
+        current.withdraw(200);
+        current.applyInterest();
+        current.display();
     }
 }
