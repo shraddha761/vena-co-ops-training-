5e27e93934f44c5b07fe7e0f95ff73bca83d69c5
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-11 12:05:50 +0530

    Update ManagerOrder.java

--- CollectionsFrameWork/Q14RestaurantOrderSystem/ManagerOrder.java
+++ CollectionsFrameWork/Q14RestaurantOrderSystem/ManagerOrder.java
@@ -17,15 +17,21 @@ public class ManagerOrder {
     }
 
     void processOrderFromFront(){
-        while(!currentOrders.isEmpty()){
+        if (!currentOrders.isEmpty()) {
             Order order = currentOrders.pollFirst();
             orderHistory.add(order);
+            System.out.println("Processed from front: " + order);
+        } else {
+            System.out.println("No orders to process from front.");
         }
     }
     void processOrderFromBack(){
-        while(!currentOrders.isEmpty()){
-            Order order = currentOrders.pollLast();  
+        if (!currentOrders.isEmpty()) {
+            Order order = currentOrders.pollLast();
             orderHistory.add(order);
+            System.out.println("Processed from back: " + order);
+        } else {
+            System.out.println("No orders to process from back.");
         }
     }
     void showCurrentOrders(){
