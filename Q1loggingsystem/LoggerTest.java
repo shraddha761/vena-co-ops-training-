bd9706f9f5a5182a386cfa4ece5548733e0ab6c0
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2025-05-11 13:59:02 +0530

    Update LoggerTest.java

--- Q1loggingsystem/LoggerTest.java
+++ Q1loggingsystem/LoggerTest.java
@@ -3,6 +3,7 @@ package Q1loggingsystem;
 public class LoggerTest {
     public static void main(String[] args) throws Exception {
         Logger logger = new Logger(true, "logs.txt"); // File based flushing
+        // Logger logger = new Logger();  // Log to console
 
         int threadCount = 100;
         int messagePerThread = 100;
